4f5dfc6ff537c6673f7ba789238627437560bc9c
Philippe Gonse <pgonse@gmail.com>
2021-04-19 17:38:41 -0400

    handle ingredient and description for update page

--- pages/api/update/[id].js
+++ pages/api/update/[id].js
@@ -8,16 +8,7 @@ export default async (req, res) => {
     const body = req.body;
     const parseBody = JSON.parse(body)
     const { id } = req.query
-    const updateRecipe = {
-        name: parseBody.name,
-        ingredient: parseBody.ingredient,
-        time: parseBody.time,
-        level: parseBody.level,
-        tag: parseBody.tag,
-        description: parseBody.description,
-        user: parseBody.user
-    }
-    Recipe.findByIdAndUpdate(id, updateRecipe)
-    .then(data => res.status(200).send(data))
-    .catch(err => res.status(400).send('unable to update to database'))
+    Recipe.findByIdAndUpdate(id, parseBody)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(400).send('unable to update to database'))
 }
--- pages/cookbook/[name]/update.js
+++ pages/cookbook/[name]/update.js
@@ -1,10 +1,19 @@
 import { UserContext } from '../../../utils/userContext'
 import { useContext, useEffect, useState } from 'react'
 import Menu from '../../../components/Menu'
+import style from '../../../styles/Update.module.scss'
 
 export default function Update() {
     const userName = useContext(UserContext);
-    const [recipe, setRecipe] = useState({})
+    const [ing, setIng] = useState('')
+    const [recipe, setRecipe] = useState({
+        name: '',
+        time: '',
+        level: '',
+        tag: '',
+        ingredient: [],
+        description: []
+    })
 
     useEffect(() => {
         if (userName) {
@@ -17,38 +26,117 @@ export default function Update() {
         const res = await fetch(`http://localhost:3000/api/${userName.email}/${recipeName[recipeName.length - 2]}`)
         const { data } = await res.json()
         setRecipe(data[0])
+        setIng(data[0].ingredient.join())
     }
 
-    const handleClick = async () => {
-        let steps = []
-        const desObj = recipe.description[0]
-        Object.keys(desObj).forEach(key => {
-            steps.push(desObj[key])
-        })
-        const newRecipe = {
-            name: recipe.name,
-            ingredient: recipe.ingredient,
-            time: recipe.time,
-            level: recipe.level,
-            tag: recipe.tag,
-            description: steps,
-            user: recipe.user
-        }
+    const handleClick = () => {
+        const newEl = recipe.description
+        newEl.push('')
+        setRecipe(prevRecipe => ({ ...prevRecipe, description: newEl }))
+    }
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const newRecipe = recipe
+        const newIng = ing.split(',')
+        newRecipe.ingredient = newIng
+        const filterDescription = recipe.description.filter(step => step !== '')
+        newRecipe.description = filterDescription
         try {
             const res = await fetch(`http://localhost:3000/api/update/${recipe._id}`, {method: 'POST',body: JSON.stringify(newRecipe)})
             const data = await res.json()
-            console.log(data)
+            window.alert('Recette actualisée')
         } catch (err) {
             console.log(err)
         }
     }
 
+    const handleChange = (e) => {
+        if (e.target.name === 'name') {
+            setRecipe(prevRecipe => ({ ...prevRecipe, name: e.target.value }))
+        } else if (e.target.name === 'time') {
+            setRecipe(prevRecipe => ({ ...prevRecipe, time: e.target.value }))
+        } else if (e.target.name === 'tag') {
+            setRecipe(prevRecipe => ({ ...prevRecipe, tag: e.target.value }))
+        } else if (e.target.name === 'level') {
+            setRecipe(prevRecipe => ({ ...prevRecipe, level: e.target.value }))
+        } else if (e.target.name === 'ingredient') {
+            setIng(e.target.value)
+        } 
+    }
+
+    const handleStepChange = (e) => {
+        let newSteps = recipe.description
+        newSteps.splice(e.target.name, 1, e.target.value)
+        setRecipe((prevRecipe) => ({ ...prevRecipe, description: newSteps}))
+    }
+
+    const displaySteps = (n, index) => {
+        return (
+            <div className={style.stepContent} key={index}>
+                <label>Step {index + 1}: </label>
+                <div className={style.styleInput}>
+                    <input type='text' name={index} value={recipe.description[index]} onChange={handleStepChange} autoComplete='off' />
+                    <label className={style.label}>
+                    </label>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <Menu>
-            <h1>
-                hello from update
-            </h1>
-            <button onClick={handleClick}>change</button>
+            <div className={style.updatePage}>
+                <button onClick={handleClick}>Retour</button>
+                <h1 style={{ textAlign: "center" }}>Update Recipe</h1>
+                <div>
+                    <form onSubmit={handleSubmit} className={style.newForm}>
+                        <div className={style.styleInput}>
+                            <input type="text" name='name' value={recipe.name} onChange={handleChange} autoComplete='off' required />
+                            <label className={style.label}>
+                                <span className={style.spanContent}>Name</span>
+                            </label>
+                        </div>
+                        <div className={style.styleInput}>
+                            <input type="text" name='ingredient' value={ing} onChange={handleChange} autoComplete='off' required />
+                            <label className={style.label}>
+                                <span className={style.spanContent}>Ingredient</span>
+                            </label>
+                        </div>
+                        <div className='time-level'>
+                            <div className={style.styleInput}>
+                                <input type='text' name='time' value={recipe.time} onChange={handleChange} autoComplete='off' required />
+                                <label className={style.label}>
+                                    <span className={style.spanContent}>Time</span>
+                                </label>
+                            </div>
+                            <div className={style.styleInput}>
+                                <input type='text' name='level' value={recipe.level} onChange={handleChange} autoComplete='off' required />
+                                <label className={style.label}>
+                                    <span className={style.spanContent}>Level</span>
+                                </label>
+                            </div>
+                        </div>
+                        <div className={style.styleInput}>
+                            <input type='text' name='tag' value={recipe.tag} onChange={handleChange} autoComplete='off' required />
+                            <label className={style.label}>
+                                <span className={style.spanContent}>Tag</span>
+                            </label>
+                        </div>
+                        <div id='steps'>
+                            {
+                                recipe.description.map((n, index) => {
+                                    return (
+                                        displaySteps(n, index)
+                                    );
+                                })
+                            }
+                        </div>
+                        <input type='submit' value='Update recipe' required />
+                    </form>
+                    <button onClick={handleClick}>add element</button>
+                </div>
+            </div>
         </Menu>
     )
 }
